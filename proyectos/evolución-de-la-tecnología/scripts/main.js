7e9d85fec358e0d9d766e35cb97c0b3b3d224245
wwuxs <wwux.renteria@gmail.com>
2024-01-13 22:42:52 -0600

    #actualización-evolución-de-la-tecnología

--- proyectos/evolución-de-la-tecnología/scripts/main.js
+++ proyectos/evolución-de-la-tecnología/scripts/main.js
@@ -2,7 +2,16 @@ const root = document.getElementById('screens')
 const btnPrev = document.getElementById('btn-prev')
 const btnNext = document.getElementById('btn-next')
 
-const w = window.innerWidth
+let index = Math.floor(root.scrollLeft / innerWidth);
 
-btnNext.addEventListener('click', () => root.scrollTo(root.scrollLeft+w, root.scrollTop))
-btnPrev.addEventListener('click', () => root.scrollTo(root.scrollLeft-w, root.scrollTop))
+const goToIndex = (value) => {
+  if(value + index <= 20 && value + index >= 0) index += value 
+  root.scrollTo(index * innerWidth, 0)
+}
+
+root.addEventListener('resize', () => {
+  index = Math.floor(root.scrollLeft / innerWidth)
+})
+
+btnNext.addEventListener('click', () => goToIndex(1))
+btnPrev.addEventListener('click', () => goToIndex(-1))
